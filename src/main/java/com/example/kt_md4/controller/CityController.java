ed68f175584244d47b015220a6f6a65b8799e84e
vudinhson2001 <you@example.com>
2022-07-12 10:17:54 +0700

    done

--- src/main/java/com/example/kt_md4/controller/CityController.java
+++ src/main/java/com/example/kt_md4/controller/CityController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @CrossOrigin("*")
@@ -21,5 +21,30 @@ public class CityController {
     @GetMapping
     public ResponseEntity<Iterable<City>> findAll(){
         return new ResponseEntity<>(cityService.findAll(), HttpStatus.OK);
+    }
+    @PostMapping()
+    public ResponseEntity<Iterable<City>> add(@Valid @RequestBody City city) {
+        cityService.save(city);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<City>> findById(@PathVariable long id) {
+        return new ResponseEntity<>(cityService.findById(id), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<City> edit(@PathVariable long id, @RequestBody City city) {
+        Optional<City> product1 = cityService.findById(id);
+        if (!product1.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        city.setId(product1.get().getId());
+        cityService.save(city);
+        return new ResponseEntity<>(city,HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<City> update(@PathVariable long id){
+        cityService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
